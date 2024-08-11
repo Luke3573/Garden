66260e1b7181d99f66e1dd2153de590b91dc3d87
Luke3573 <132735214+Luke3573@users.noreply.github.com>
2024-08-11 22:07:20 +0800

    Update bed3.py
    updated code with mem clear function to stop code from stalling

--- bed3.py
+++ bed3.py
@@ -1,5 +1,9 @@
 def main():
+    print ("start")
     import RPi.GPIO as GPIO
+    from urllib3 import disable_warnings
+    from urllib3.exceptions import InsecureRequestWarning
+    disable_warnings(InsecureRequestWarning)
     import pyrebase
     import time
     import serial
@@ -18,17 +22,20 @@ def main():
     }
 
     firebase1 = pyrebase.initialize_app(config1)
-
+    print ("firebase get")
     if True:
         tempA = sensorA.readTempC()
         time.sleep(1)
+        print ("temp read")
         if tempA is not None:
             Temp = ("{:.2f}°C".format(tempA))
             temperature = Temp
             time.sleep(1)
+            print ("temp read yes")
         else:
             temperature = "FLT"
             time.sleep(3);
+            print ("temp read no")
 
     if True:
         database = firebase1.database()
@@ -36,43 +43,71 @@ def main():
         valve = gardenMonitoring.child("valveStatus").get().val()
         global valveStatus
         valveStatus = valve
+        print ("valve stat get")
 
         database = firebase1.database()
         gardenMonitoring.child("GMS").child("temperature").set(temperature)
-
+        print ("temp set")
         global systemActive
         database = firebase1.database()
         gardenMonitoring.child("GMS").child("systemActive").set(systemActive)
-
+        print ("set system")
         time.sleep(10)
 
-        else:
+    else:
+        print ("data fail")
         main()
+
     if valveStatus == "1":
         GPIO.output(17, 1)
         systemActive = "1"
-        main()
+        print ("set valve 1")
+  #      main()
     elif valveStatus == "0":
         GPIO.output(17, 0)
         systemActive = "0"
+        print ("set valve 0")
 
-        main()
+ #       main()
     else:
         time.sleep(2)
+        print ("set valve fail")
+        main()
+
+
+    if True:
+        current_time = time.time()
+        local_time = time.localtime(current_time)
+
+        hour = local_time.tm_hour
+        minute = local_time.tm_min
+
+        if hour == 23 and minute == 55:
+            call("clear", shell=True)
+            call(" sudo sh -c 'echo 1 >  /proc/sys/vm/drop_caches' ", shell=True)
+
+            time.sleep(10)
+            main()
+
+        else:
+
+            main()
+
+    else:
         main()
 
 def sched():
 #    *********time call**********
         hour = time.time()
         print(hour)
-        if hour == time1 && minute == time2:
-        database = firebase1.database()
-        gardenMonitoring.child("GMS").child("valveStatus").set("1")
-        
-    else:
-        database = firebase1.database()
-        gardenMonitoring.child("GMS").child("valveStatus").set("1")
-        main()
+        if hour == time1 :
+            database = firebase1.database()
+            gardenMonitoring.child("GMS").child("valveStatus").set("1")
+
+        else:
+            database = firebase1.database()
+            gardenMonitoring.child("GMS").child("valveStatus").set("1")
+            main()
 
 sensor = 0
 temperature = 1
@@ -82,3 +117,6 @@ systemActive = 0
 valveStatus = 0
 fans = 0
 main()
+
+
+
